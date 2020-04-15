8affe4d306d25f03c6b255d60208813b8c990c2b
Neville Omangi <user.email 31406314+nevilleomangi@users.noreply.github.com>
2020-04-15 07:40:01 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,151 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <p>DSTV Channel List</p>
+    </header>
+
+    <article class="search">
+      <input type="text" name="" id="channel-input" placeholder="NatGeo" v-model="searchTerm">
+      <p class="caption">Search for channel</p>
+    </article>
+
+    <article class="list">
+      <ul>
+        <Channel 
+          v-for="channel in filtered"
+          :channel="channel"
+          :key="channel.id"
+        > 
+        </Channel>
+      </ul>
+    </article>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Channel from './components/Channel.vue'
+
+// channels
+import access from './assets/channels/access.json'
+import compactPlus from './assets/channels/compact_plus.json'
+import compact from './assets/channels/compact.json'
+import family from './assets/channels/family.json'
+import free from './assets/channels/free.json'
+import premium from './assets/channels/premium.json'
+
+import fuzzy from 'fuzzysort'
+
+const channelNames = new Map([
+  ['premium', 'DSTV Premium',],
+  ['compact', 'DSTV Compact'],
+  ['compact_plus', 'DSTV Compact Plus'],
+  ['family', 'DSTV Family'],
+  ['access', 'DSTV Access'],
+  ['free', 'Free to View'],
+])
+
+const channelNamesArr = Array.from(channelNames.keys())
+
+// console.log(access.JSON.items[0])
+const jsons = [access, compactPlus, compact, family, free, premium]
+const channels = jsons.map(json => json.JSON.items)
+const withBouquet = channels.map((list, index) => list.map(channel => {
+  channel.channelNumber = parseInt(channel.channelNumber)
+  channel.bouquet = channelNamesArr[index]
+  return channel
+}))
+.flat(1) // join lists
+.reduce((acc, current) => { // remove duplicates
+  // returns values with no match
+  if (!acc.some(channel => channel.id === current.id)) acc.push(current)
+  return acc
+}, [])
+
+// console.log(withBouquet)
 
 export default {
   name: 'App',
+
+  data() {
+    return {
+      channels: withBouquet,
+      searchTerm: '',
+    }
+  },
+
+  computed: {
+    filtered() {
+      if (this.searchTerm === '') return this.channels
+      return fuzzy.go(this.searchTerm, this.channels, { key: 'channelName' }).map(result => result.obj)
+      // return this.channels.filter(channel => channel.channelName.toLowerCase().includes(this.searchTerm.toLowerCase()))
+    }
+  },
+
   components: {
-    HelloWorld
-  }
+    Channel,
+  },
 }
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+}
+
+body {
+  padding: 0;
+  margin: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+
+  display: flex;
+  flex-direction: column;
+}
+
+header > p {
+  font-size: 1.5em;
+  text-align: center;
+  font-weight: bold;
+  color: #0094d9;
+}
+
+.search {
+  padding: 0.5em;
+}
+
+.search > input {
+  line-height: 2em;
+  font-size: 1em;
+  background: rgba(150, 150, 150, .1);
+  padding: .5em 1em;
+  border: none;
+  border-bottom: 2px solid #0094d9;
+  /* width: 100%; */
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
+  width: 100%;
+}
+
+.caption {
+  margin: 0;
+  padding: .25em 0;
+  font-size: .8em;
+  color: rgba(150, 150, 150, .8);
+  font-style: italic;
+  text-align: right;
+}
+
+.list {
+  padding: 0.5em;
+}
+
+.list > ul {
+  list-style-type: none;
+  padding-left: 0;
 }
 </style>
---
+++ src/components/Channel.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="channel">
+    <div class="icon">
+      <img 
+        :src="channel.channelLogoPaths.SMALL" 
+        alt=""
+        width="50"
+      >
+    </div>
+    <div class="name">
+      {{ channel.channelName }}
+    </div>
+    <div class="number">
+      {{ channel.channelNumber }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Channel',
+  props: [ 'channel', ],
+}
+</script>
+
+<style scoped>
+.channel {
+  display: flex;
+  padding: 1em;
+  align-items: center;
+  /* x, y, blur, spread */
+  box-shadow: 1px 1px 10px 0px rgba(100, 100, 100, 0.5);
+  border-radius: 5px;
+  margin-bottom: 1em;
+}
+
+.channel > * {
+  padding: 0 .5em;
+}
+
+.number {
+  font-weight: 900;
+  font-size: 2em;
+}
+
+.name {
+  height: fit-content;
+  flex-grow: 2;
+}
+
+</style>
